0178c69d9458f9ecbf77a828a32cd25bfa1711b3
Tasreen Mowreen <tmowreen97@gmail.com>
2022-10-19 22:12:38 -0400

    Done.

--- control_flow.rb
+++ control_flow.rb
@@ -43,7 +43,8 @@ def calculator(operation, num1, num2)
   when "*" then num1*num2
   when "/" then num1/num2
   else 
-    puts 'Invalid operator!'
+    error = "Invalid operation!"
+    puts error
   end
 
 end
